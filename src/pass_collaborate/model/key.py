0002e7fa101d17b16985b6496dfd0a9f3995dbdd
Lyz <lyz@riseup.net>
2023-03-15 12:15:52 +0100

    ci: import annotations for python < 3.9

--- src/pass_collaborate/model/key.py
+++ src/pass_collaborate/model/key.py
@@ -1,10 +1,11 @@
 """Define the model of the key objects."""
 
 try:
-    from typing import Annotated
+    from typing import Annotated  # type: ignore
 except ImportError:
     from typing_extensions import Annotated
 
 from pydantic import Field  # noqa: E0611
 
+# mypy is complaining that it can't import Annotated, but it's solved with the except
 GPGKey = Annotated[str, Field(regex="^[0-9A-Z]{16,40}$")]
