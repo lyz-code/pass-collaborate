d3586757dce4b49f2b7f92e6da44a93c30744131
Lyz <lyz@riseup.net>
2023-04-11 14:26:03 +0200

    fix: install well the password-store extensions

--- pdm_build.py
+++ pdm_build.py
@@ -4,7 +4,7 @@ import os
 from pathlib import Path
 import shutil
 
-def build(src, dst):
+def pdm_build_update_files(context, files):
     """Create the needed files in the user's device.
 
     `pdm` will run this script when installing the application.
@@ -14,21 +14,10 @@ def build(src, dst):
         dst: the path to the distribution directory
     """
 
-    # Configure the extensions
     if 'PASSWORD_STORE_EXTENSIONS_DIR' in os.environ:
         lib = Path(os.environ['PASSWORD_STORE_EXTENSIONS_DIR'])
     else:
         lib = Path.home() / '.password-store' / '.extensions'
-    os.makedirs(lib, exist_ok=True)
 
     for file in ['user.bash', 'group.bash', 'access.bash']:
-        shutil.copyfile(f"assets/{file}", lib / file)
-        os.chmod(lib / file, 0o755)
-
-    # Enable the extensions
-    for file in ['.bashrc', '.zshrc']:
-        config = Path.home() / file
-        if config.exists():
-            if 'export PASSWORD_STORE_ENABLE_EXTENSIONS=true' not in config.read_text():
-                with config.open('a') as f:
-                    f.write('export PASSWORD_STORE_ENABLE_EXTENSIONS=true')
+        files[f"assets/{file}"] = lib / file
